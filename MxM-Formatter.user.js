a73e9d937299ca50731912438b5efaf87bd65c1a
AshtonLG3 <ngeziman@live.com>
2025-10-19 11:37:06 +0200

    Update MxM-Formatter.user.js

--- MxM-Formatter.user.js
+++ MxM-Formatter.user.js
@@ -9,7 +9,7 @@
 // @run-at       document-idle
 // @grant        none
 // @downloadURL  https://raw.githubusercontent.com/AshtonLG3/Musixmatch-In-Editor-Lyrics-Formatter/main/MxM-Formatter.user.js
-// @updateURL    https://raw.githubusercontent.com/AshtonLG3/Musixmatch-In-Editor-Lyrics-Formatter/main/MxM-Formatter.user.js
+// @updateURL    https://raw.githubusercontent.com/AshtonLG3/Musixmatch-In-Editor-Lyrics-Formatter/main/MxM-Formatter.meta.js
 // ==/UserScript==
 
 (function (global) {
