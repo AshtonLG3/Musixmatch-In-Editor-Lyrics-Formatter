749af41883f54a0183436cfd8c9adcd5e03afc56
AshtonLG3 <ngeziman@live.com>
2025-10-19 09:55:53 +0000

    Add standalone .meta.js manifest for Tampermonkey

---
+++ MxM-Formatter.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         MxM In-Editor Formatter (EN)
+// @namespace    mxm-tools
+// @version      1.1.2
+// @description  Musixmatch Studio-only formatter with improved BV, punctuation, and comma relocation fixes
+// @author       Vincas Stepankevičius & Richard Mangezi Muketa
+// @match        https://curators.musixmatch.com/*
+// @match        https://curators-beta.musixmatch.com/*
+// @run-at       document-idle
+// @grant        none
+// @downloadURL  https://raw.githubusercontent.com/AshtonLG3/Musixmatch-In-Editor-Lyrics-Formatter/main/MxM-Formatter.user.js
+// @updateURL    https://raw.githubusercontent.com/AshtonLG3/Musixmatch-In-Editor-Lyrics-Formatter/main/MxM-Formatter.meta.js
+// ==/UserScript==
