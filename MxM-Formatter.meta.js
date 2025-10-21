278de9e7f0f05975144c215d37f6ad69fb6acf8c
AshtonLG3 <ngeziman@live.com>
2025-10-21 08:06:16 +0000

    Apply patch: update MxM In-Editor Formatter (EN) to v1.1.12

--- MxM-Formatter.meta.js
+++ MxM-Formatter.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MxM In-Editor Formatter (EN)
 // @namespace    mxm-tools
-// @version      1.1.11
+// @version      1.1.12
 // @description  Musixmatch Studio-only formatter with improved BV, punctuation, and comma relocation fixes
 // @author       Vincas Stepankevičius & Richard Mangezi Muketa
 // @match        https://curators.musixmatch.com/*
