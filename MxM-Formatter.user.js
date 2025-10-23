ab020f4c0dd2cc0115afabbee32d7250a075e917
AshtonLG3 <ngeziman@live.com>
2025-10-23 09:35:47 +0200

    Enable floating button in extension defaults

--- MxM-Formatter.user.js
+++ MxM-Formatter.user.js
@@ -24,7 +24,7 @@
     lang: "EN",
     autoLowercase: false,
     fixBackingVocals: true,
-    showFloatingButton: false
+    showFloatingButton: true
   };
   const extensionOptions = { ...extensionDefaults };
 
