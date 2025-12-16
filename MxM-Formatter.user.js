24663270758670af590e8642d2d0e7035f6eba48
mangezi <ngeziman@live.com>
2025-12-16 06:02:28 -0800

    Update MxM-Formatter.user.js

--- MxM-Formatter.user.js
+++ MxM-Formatter.user.js
@@ -1823,7 +1823,7 @@ const WELL_CLAUSE_STARTERS = new Set([
         "pumpkin","grin","chin","twin","skin","basin","raisn","spain","birkin","balmain","boutin","login","pin","curtain",
         "fin","din","min","gin","lin","kin","sin","win","bin","thin","tin","akin","leadin","captain","mountain",
         "fountain","certain","again","gain","spin","twin","main","cain","maintain","retain","detain","vain","regain",
-        "rain","brain","pain","drain","train","grain","cabin","satin","chain","plain","remain","campaign",
+        "rain","brain","pain","drain","train","grain","cabin","satin","chain","robin","kevin","kelvin","plain","remain","campaign",
         "fein","contain","domain","explain","sustain","pertain","obtain","entertain","villain","admin","abstain","stain"
       ]);
 
